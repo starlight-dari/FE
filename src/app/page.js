5b4a3401d74e14a28de6e4e79531d7cdb60fd61f
jooyeong <juyeong@sookmyung.ac.kr>
2025-01-09 15:52:47 +0900

    카카오로그인 임시 푸시

--- src/app/page.js
+++ src/app/page.js
@@ -10,17 +10,19 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/starlight-logo.png"
+          alt="별빛다리 로고"
+          width={270}
+          height={259}
           priority
         />
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <button type="button" onClick={() => router.push("/login")}>
-            LogIn
+            카카오로 로그인하기
+          </button>
+          <button type="button" onClick={() => router.push("/mypage")}>
+            마이페이지
           </button>
         </div>
       </main>
