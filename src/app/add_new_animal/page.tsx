c0475debbcd382e8fc12f0a7fd4cc01bfc599758
jooyeong <juyeong@sookmyung.ac.kr>
2025-03-06 21:10:41 +0900

    feat: 동물 추가페이지에 이미지 업로드 컴포넌트 적용

--- src/app/add_new_animal/page.tsx
+++ src/app/add_new_animal/page.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 import styled from "styled-components";
 import React, { useState } from "react";
 import Header from "../../components/header";
+import ImageUpload from "../../components/imageUpload";
 
 export default function Page() {
   const [genderSelected, setGenderSelected] = useState("");
@@ -24,90 +25,88 @@ export default function Page() {
     <>
       <Header />
       <Body>
-        <Container>
-          <PhotoBox />
-          <ItemWrapper>
-            <Title>새 별자리 만들기</Title>
-            <Item>
-              <Label style={{ paddingRight: "24px" }}>이름</Label>
-              <Input placeholder="반려동물의 이름으로 별자리가 만들어질 거에요." />
-            </Item>
-            <Item>
-              <Label style={{ paddingRight: "38px" }}>종</Label>
-              <Input placeholder="종을 적어주세요. (예: 강아지, 고양이)" />
-            </Item>
-            <Item>
-              <Label style={{ paddingRight: "24px" }}>성별</Label>
-              <GenderButton>
-                <input
-                  type="radio"
-                  id="male"
-                  name="male"
-                  value="male"
-                  checked={genderSelected === "male"}
-                  onChange={(e) => setGenderSelected(e.target.value)}
-                />
-                <label htmlFor="male">남자</label>
-                <input
-                  type="radio"
-                  id="female"
-                  name="female"
-                  value="female"
-                  checked={genderSelected === "female"}
-                  onChange={(e) => setGenderSelected(e.target.value)}
-                />
-                <label htmlFor="female">여자</label>
-                <input
-                  type="radio"
-                  id="undefined"
-                  name="undefined"
-                  value="undefined"
-                  checked={genderSelected === "undefined"}
-                  onChange={(e) => setGenderSelected(e.target.value)}
-                />
-                <label htmlFor="undefined">모르겠어요</label>
-              </GenderButton>
-            </Item>
-            <Item>
-              <Label style={{ paddingRight: "30px" }}>태어난 날</Label>
-              <DateInput
-                type="date"
-                value={birthDate}
-                max={today}
-                onChange={(e) => setBirthDate(e.target.value)}
+        <ImageUpload />
+        <ItemWrapper>
+          <Title>새 별자리 만들기</Title>
+          <Item>
+            <Label style={{ paddingRight: "24px" }}>이름</Label>
+            <Input placeholder="반려동물의 이름으로 별자리가 만들어질 거에요." />
+          </Item>
+          <Item>
+            <Label style={{ paddingRight: "38px" }}>종</Label>
+            <Input placeholder="종을 적어주세요. (예: 강아지, 고양이)" />
+          </Item>
+          <Item>
+            <Label style={{ paddingRight: "24px" }}>성별</Label>
+            <GenderButton>
+              <input
+                type="radio"
+                id="male"
+                name="male"
+                value="male"
+                checked={genderSelected === "male"}
+                onChange={(e) => setGenderSelected(e.target.value)}
               />
-            </Item>
-            <Item>
-              <Label style={{ paddingRight: "26px" }}>별이 된 날</Label>
-              <DateInput
-                type="date"
-                value={starDate}
-                max={today}
-                onChange={(e) => setStarDate(e.target.value)}
+              <label htmlFor="male">남자</label>
+              <input
+                type="radio"
+                id="female"
+                name="female"
+                value="female"
+                checked={genderSelected === "female"}
+                onChange={(e) => setGenderSelected(e.target.value)}
               />
-            </Item>
-            <Item>
-              <Label style={{ paddingRight: "62px" }}>성격</Label>
-              <SelectWrapper>
-                <SelectedOption onClick={() => setIsOpen(!isOpen)}>
-                  {personalitySelected}
-                </SelectedOption>
-                <OptionsList isOpen={isOpen}>
-                  <OptionItem onClick={() => handleSelect("활발함")}>
-                    활발함
-                  </OptionItem>
-                  <OptionItem onClick={() => handleSelect("조용함")}>
-                    조용함
-                  </OptionItem>
-                  <OptionItem onClick={() => handleSelect("귀여움")}>
-                    귀여움
-                  </OptionItem>
-                </OptionsList>
-              </SelectWrapper>
-            </Item>
-          </ItemWrapper>
-          <Button>새 별자리 만들기</Button>
-        </Container>
+              <label htmlFor="female">여자</label>
+              <input
+                type="radio"
+                id="undefined"
+                name="undefined"
+                value="undefined"
+                checked={genderSelected === "undefined"}
+                onChange={(e) => setGenderSelected(e.target.value)}
+              />
+              <label htmlFor="undefined">모르겠어요</label>
+            </GenderButton>
+          </Item>
+          <Item>
+            <Label style={{ paddingRight: "30px" }}>태어난 날</Label>
+            <DateInput
+              type="date"
+              value={birthDate}
+              max={today}
+              onChange={(e) => setBirthDate(e.target.value)}
+            />
+          </Item>
+          <Item>
+            <Label style={{ paddingRight: "26px" }}>별이 된 날</Label>
+            <DateInput
+              type="date"
+              value={starDate}
+              max={today}
+              onChange={(e) => setStarDate(e.target.value)}
+            />
+          </Item>
+          <Item>
+            <Label style={{ paddingRight: "62px" }}>성격</Label>
+            <SelectWrapper>
+              <SelectedOption onClick={() => setIsOpen(!isOpen)}>
+                {personalitySelected}
+              </SelectedOption>
+              <OptionsList isOpen={isOpen}>
+                <OptionItem onClick={() => handleSelect("활발함")}>
+                  활발함
+                </OptionItem>
+                <OptionItem onClick={() => handleSelect("조용함")}>
+                  조용함
+                </OptionItem>
+                <OptionItem onClick={() => handleSelect("귀여움")}>
+                  귀여움
+                </OptionItem>
+              </OptionsList>
+            </SelectWrapper>
+          </Item>
+        </ItemWrapper>
+        <Button>새 별자리 만들기</Button>
       </Body>
     </>
   );
@@ -115,29 +114,14 @@ export default function Page() {
 
 const Body = styled.div`
   display: flex;
-  justify-content: center;
-`;
-
-const Container = styled.div`
-  width: 90vw;
-  height: 565px;
-  background-color: tansparent;
+  padding: 30px;
   color: white;
   position: relative;
-  display: flex;
   align-items: center;
-  padding: 0 10px;
-`;
-
-const PhotoBox = styled.div`
-  width: 460px;
-  height: 535px;
-  background-color: #ece6f0;
-  border: 1px dashed #65558f;
+  gap: 150px;
 `;
 
 const ItemWrapper = styled.div`
-  margin-left: 38px;
   display: flex;
   flex-direction: column;
   gap: 15px;
@@ -238,5 +222,5 @@ const Button = styled.button`
   cursor: pointer;
   position: absolute;
   bottom: 25px;
-  right: 34px;
+  right: 70px;
 `;
