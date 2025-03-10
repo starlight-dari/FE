53852fcd2457d546b0eb8dc86ab0fe584af987ab
jooyeong <juyeong@sookmyung.ac.kr>
2025-03-11 00:55:30 +0900

    feat: 마이페이지 유저 프로필 사진 api 연결

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "starlightbucket.s3.ap-northeast-2.amazonaws.com", // ✅ 도메인 설정
+        port: "",
+        pathname: "/petImgs/**", // ✅ 경로 설정
+      },
+      { protocol: "http", hostname: "img1.kakaocdn.net" },
+    ],
+  },
 };
 
 export default nextConfig;
