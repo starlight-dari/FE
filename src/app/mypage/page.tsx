7dd06a384c450a8e075cc896c96cd446566fef13
jooyeong <juyeong@sookmyung.ac.kr>
2025-03-10 22:57:50 +0900

    feat: 마이페이지 유저정보, 메뉴 UI 제작

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { useRouter } from "next/navigation";
+import Header from "../../components/header";
+import Image from "next/image";
+
+import UserInfo from "../../components/userInfo";
+
+export default function Page() {
+  const router = useRouter();
+
+  return (
+    <>
+      <Header />
+      <Body>
+        <MenuBar>
+          <Menu>나의 별자리</Menu>
+          <Menu>추억별 모아보기</Menu>
+          <Menu>나의 후기 모음</Menu>
+        </MenuBar>
+        <Container>
+          <UserInfo />
+        </Container>
+      </Body>
+    </>
+  );
+}
+
+const Body = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  border-top: 1px solid #fff;
+  position: absolute;
+  top: 65px;
+  color: #fff;
+`;
+
+const MenuBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: calc(100vh - 65px);
+  top: 65px;
+  padding: 20px 0 0 20px;
+  gap: 25px;
+  border-right: 1px solid #fff;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  font-size: 27px;
+  font-weight: 900;
+`;
+
+const Container = styled.div``;
---
+++ src/components/userInfo.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+
+import JJONG from "/public/jjong.svg";
+import edit from "/public/edit.svg";
+
+const UserInfo = () => {
+  return (
+    <Container>
+      <UserImage src={JJONG} alt="user photo" />
+      <UserWrapper>
+        <UserDetail>
+          <User>
+            <UserName>백주영</UserName>
+            <span>님</span>
+          </User>
+          <UserEmail>example@domain.com</UserEmail>
+        </UserDetail>
+        <UserNickName>
+          닉네임: 134435
+          <EditButton>
+            <Image src={edit} alt="edit" width={12} height={12} />
+          </EditButton>
+        </UserNickName>
+      </UserWrapper>
+      <MemoryStar>추억별 34</MemoryStar>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  width: calc(100vw - 360px);
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+  border-bottom: 1px solid #fff;
+  position: relative;
+`;
+
+const UserImage = styled(Image)`
+  width: 70px;
+  height: 70px;
+  border-radius: 100px;
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`;
+
+const UserDetail = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const User = styled.div`
+  display: flex;
+  align-items: baseline;
+  gap: 7px;
+  font-size: 13px;
+`;
+
+const UserName = styled.span`
+  font-weight: 900;
+  font-size: 17px;
+`;
+
+const UserEmail = styled.div`
+  color: #d9d9d9;
+  font-size: 12px;
+`;
+
+const UserNickName = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const EditButton = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
+
+const MemoryStar = styled.div`
+  position: absolute;
+  right: 100px;
+`;
+
+export default UserInfo;
