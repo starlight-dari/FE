85f4860203d51c34ef34e9a8a4be5da704d258c8
jooyeong <juyeong@sookmyung.ac.kr>
2025-03-21 03:07:10 +0900

    feat: 마이페이지 분리 #46

--- src/app/mypage/myInfo/page.tsx
+++ src/app/mypage/myInfo/page.tsx
@@ -2,9 +2,9 @@
 
 import React from "react";
 import styled from "styled-components";
-import Header from "../../components/header";
-import UserInfo from "../../components/userInfo";
-import MyStar from "../../components/myStar";
+import Header from "../../../components/header";
+import UserInfo from "../../../components/userInfo";
+import MyStar from "../../../components/myStar";
 
 export default function Page() {
   return (
