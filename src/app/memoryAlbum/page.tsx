8dbf1089b5905e29b7dd7906c9f3fb3aa040af96
jooyeong <juyeong@sookmyung.ac.kr>
2025-03-23 02:14:26 +0900

    feat: 추억앨범 상세정보페이지 연결 테스트 #59

--- src/app/memoryAlbum/page.tsx
+++ src/app/memoryAlbum/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import styled from "styled-components";
-import { useRouter } from "next/navigation";
-import React, { useState, useEffect } from "react";
-import Header from "../../components/header";
-import AlbumContent from "../../components/memoryAlbumContent";
-import AlbumList from "../../components/memoryAlbumList";
+import React from "react";
 
 export interface AlbumData {
   petId: number;
@@ -26,43 +22,16 @@ export interface PetAlbumContent {
 }
 
 const Page: React.FC = () => {
-  const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
-  const [selectedPetId, setSelectedPetId] = useState<number | null>(null);
-
-  const handlePetAlbum = (petId: number) => {
-    setSelectedPetId(petId);
-  };
-
   return (
     <>
-      <Header />
-      <Body>
-        <AlbumList onSelectPet={handlePetAlbum} />
-        {selectedPetId ? (
-          <AlbumContent petId={selectedPetId} />
-        ) : (
-          <Container>
-            <h1>
-              왼쪽에서 반려동물을 선택해서 <br />
-              반려동물로부터 도착한 편지를 확인해보세요.
-            </h1>
-          </Container>
-        )}
-      </Body>
+      <Container>
+        <h1>반려동물을 선택하여 반려동물로부터 도착한 편지를 확인해보세요.</h1>
+      </Container>
     </>
   );
 };
 
 export default Page;
-
-const Body = styled.div`
-  display: flex;
-  height: calc(-105px + 100vh);
-  justify-content: center;
-  position: absolute;
-  top: 100px;
-  color: #fff;
-`;
 
 const Container = styled.div`
   display: flex;
